71f0c0183d1ea105bb97a32867e71b3b77f7f418
Maxine Ludington <maxineludington@maxines-macbook-air.local>
2016-07-20 22:09:28 -0500

    Initial Commit

---
+++ Homework2c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework2c
+//
+//  Created by Maxine Ludington on 7/20/16.
+//  Copyright © 2016 Maxine Ludington. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
